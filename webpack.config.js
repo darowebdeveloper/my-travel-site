45816bbd49c0b4219933da1e6c89a1a90ac8287d
daro phan <darowebdeveloper@gmail.com>
2020-05-08 20:03:24 +0700

    finished hero and two pictures parts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 const postCSSPlugins = [
-    require('postcss-nested'),
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('autoprefixer'),
     require('postcss-simple-vars'),
-    require('autoprefixer')
+    require('postcss-nested'),
 ];
 
 module.exports = {
@@ -12,11 +14,32 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
-            { test: /\.css$/i, use: ['style-loader','css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}] }
+            { 
+                test: /\.css$/i, 
+                use: 
+                [
+                    'style-loader',
+                    'css-loader?url=false', 
+                    {
+                        loader: 'postcss-loader', 
+                        options: {
+                            plugins: postCSSPlugins
+                        }
+                    }
+                ] 
+            }
         ]
     }
 }
