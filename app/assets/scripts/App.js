f094832169a4908f3941fe1eba351b82ae90557c
daro phan <darowebdeveloper@gmail.com>
2020-05-31 18:39:30 +0700

    added commented notes in the files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,10 @@
+// Import style.css for webpack to bundle the css file <=> add config to webpack
+// When ready for productions, will export css to its own files
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu();
+// For hot module replacement with webpack-dev-server
 if(module.hot) {
     module.hot.accept();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+// Require path from node.js
 const path = require('path');
-
+// PostCSS plugins for webpack...
+// npm i all of the following --save-dev
 const postCSSPlugins = [
   require("postcss-import"),
   require("postcss-mixins"),
@@ -8,39 +10,57 @@ const postCSSPlugins = [
   require("postcss-nested"),
   require("postcss-hexrgba"),
 ];
-
+// Export the {} for webpack npm i webpack webpack-cli --save-dev
+// Inside package.json => "script": {"dev":"webpack"} for no webpack-dev-server
 module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
+  // Where the starting script
+  entry: "./app/assets/scripts/App.js",
+  // After bundling what the name and location
+  output: {
+    // Name after bundling
+    filename: "bundled.js",
+    // Absolute location to store the bundled by using path.resolve
+    path: path.resolve(__dirname, "app"),
+  },
+  // Use devSErver -> npm i webpack-dev-server --save-dev => "script":{"dev":"webpack-dev-server"}
+  devServer: {
+    // Watch & reload the changes made to html files
+    before: function (app, server) {
+      server._watch("./app/**/*.html");
     },
-    devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            { 
-                test: /\.css$/i, 
-                use: 
-                [
-                    'style-loader',
-                    'css-loader?url=false', 
-                    {
-                        loader: 'postcss-loader', 
-                        options: {
-                            plugins: postCSSPlugins
-                        }
-                    }
-                ] 
-            }
-        ]
-    }
-}
+    // Where to serve to the output bundled directory by using path.join
+    contentBase: path.join(__dirname, "app"),
+    // Use hot module replacing (inject css & js) <=> need to add module.hot in the App.js file
+    hot: true,
+    port: 3000,
+    // For local hosting with local network
+    host: "0.0.0.0",
+  },
+  // Required by webpack 
+  mode: "development",
+  // Where to put additional plugins for webpacks like css
+  module: {
+    rules: [
+      {
+        // For all css files using regx
+        test: /\.css$/i,
+        // Use the following plugins when webpack see .css file in App.js
+        use: [
+          // npm i style-loader --save-dev for parsing styles in browsers & in App.js
+          "style-loader",
+          // npm i css-loader --save-dev for loading css in App.js
+          // ?url=false not to handle images in css file like background-image
+          "css-loader?url=false",
+          {
+            // npm i postcss-loader --save-dev to understand postcss
+            loader: "postcss-loader",
+            options: {
+              // Many plugins for postcss-loader
+              plugins: postCSSPlugins,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
