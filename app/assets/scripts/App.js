4e32c1d35a9b6192d1983172fddd31e726fadd09
daro phan <darowebdeveloper@gmail.com>
2020-06-06 21:25:10 +0700

    finihsed overlay modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,8 +5,10 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // Create the object
+new Modal();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75 );
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let stickyHeader = new StickyHeader();
---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,60 @@
+class Modal {
+  constructor() {
+    // Call the injectHTML func when instantiation
+    // Order matters here, meaning first injectHTML then call the events
+    this.injectHTML();
+    this.modal = document.querySelector('.modal');
+    this.closeIcon = document.querySelector('.modal__close');
+    this.openModalButtons = document.querySelectorAll('.open-modal');
+    this.events();
+  }
+  events() {
+    // Listen for open click
+    this.openModalButtons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        this.openTheModal(e);
+      });
+    });
+    // Listen for closing click
+    this.closeIcon.addEventListener('click', (button) => {
+      this.closeTheModal();
+    });
+    // Listen for esc key
+    document.addEventListener('keyup', (e) => this.keyPressHandler());
+  }
+  keyPressHandler(e) {
+    // If esc keey is pressed
+    if(e.keyCode == 27) {
+      this.closeTheModal();
+    }
+  }
+  openTheModal(e) {
+    // To prevent the # click defaul behavior
+    e.preventDefault();
+    this.modal.classList.add('modal--is-visible');
+  }
+  closeTheModal() {
+    this.modal.classList.remove('modal--is-visible');
+  }
+  injectHTML() {
+    // first argument where to insert and the second the html
+    document.body.insertAdjacentHTML('beforeend',`
+      <div class="modal">
+        <div class="modal__inner">
+          <h2 class="section-title section-title--blue section-title--less-margin"><img src="assets/images/icons/mail.svg" class="section-title__icon"> Get in <strong>Touch</strong></h2>
+          <div class="wrapper wrapper--narrow">
+            <p class="modal__description">We will have an online order system in place soon. Until then, connect with us on any of the platforms below!</p>
+          </div>
+          <div class="social-icons">
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/facebook.svg" alt="Facebook"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/twitter.svg" alt="Twitter"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/instagram.svg" alt="Instagram"></a>
+            <a href="#" class="social-icons__icon"><img src="assets/images/icons/youtube.svg" alt="YouTube"></a>
+          </div>
+        </div>
+        <div class="modal__close">X</div>
+      </div>
+    `);
+  }
+}
+export default Modal;
