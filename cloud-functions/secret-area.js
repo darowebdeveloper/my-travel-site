239eb557d4fd39fbfbcf791c4d991a9ebf2a091b
daro phan <darowebdeveloper@gmail.com>
2020-06-07 14:33:45 +0700

    updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area."
-  });
+  const secretContent = `
+    <h3>Welcome to secret area</h3>
+    <p>This is a password-protected area. You are seeing meaning your password is <strong>hacked</strong>.</p>
+  `;
+  let body;
+  // event.body is literal text sent by user
+  if(event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if(body.password == 'javascript' ) {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      // Unauthorized 401
+      statusCode: 401,
+    });
+  }
 };
